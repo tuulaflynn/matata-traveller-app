98a17ebb14a9b6c8026b876c976835f6d3e948c4
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-22 13:43:46 +0100

    CategoryService interface

--- src/main/java/com/aalifadesigns/matatatraveller/service/CategoryService.java
+++ src/main/java/com/aalifadesigns/matatatraveller/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.aalifadesigns.matatatraveller.service;
 
+import com.aalifadesigns.matatatraveller.model.CategoryDto;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    public List<CategoryDto> fetchAllCategories();
+    public CategoryDto fetchACategory (int categoryId);
+
 }
