cbb708afb0483a588b67f5415098a0115c6a8228
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-22 13:48:47 +0100

    CategoryServiceImpl initial setup - constructor DI

--- src/main/java/com/aalifadesigns/matatatraveller/service/CategoryServiceImpl.java
+++ src/main/java/com/aalifadesigns/matatatraveller/service/CategoryServiceImpl.java
@@ -1,4 +1,29 @@
 package com.aalifadesigns.matatatraveller.service;
 
-public class CategoryServiceImpl {
+import com.aalifadesigns.matatatraveller.dao.CategoryDao;
+import com.aalifadesigns.matatatraveller.model.CategoryDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    CategoryDao categoryDao;
+
+    @Autowired //Constructor Dependency Injection
+    public CategoryServiceImpl(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @Override
+    public List<CategoryDto> fetchAllCategories() {
+        return null;
+    }
+
+    @Override
+    public CategoryDto fetchACategory(int categoryId) {
+        return null;
+    }
 }
