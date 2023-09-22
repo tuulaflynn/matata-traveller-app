5b1c713851f3601b8094cf12dd7ef68b1b3d104c
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-22 13:36:58 +0100

    CategoryDao

--- src/main/java/com/aalifadesigns/matatatraveller/dao/CategoryDao.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/CategoryDao.java
@@ -1,4 +1,7 @@
 package com.aalifadesigns.matatatraveller.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CategoryDao {
 }
--- src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CategoryEntity.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CategoryEntity.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import java.util.List;
 
+//Using Lombok
 @NoArgsConstructor //empty constructor
 @AllArgsConstructor //a constructor using all attributes
 @Getter //for getters
