4398d6ccb2094383f39cfe6078b2ae0c4230b439
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-23 07:30:41 +0100

    DAO layer completed for both entities(City, Category)

--- src/main/java/com/aalifadesigns/matatatraveller/dao/CategoryDao.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/CategoryDao.java
@@ -1,7 +1,9 @@
 package com.aalifadesigns.matatatraveller.dao;
 
+import com.aalifadesigns.matatatraveller.dao.entities.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryDao {
+public interface CategoryDao extends JpaRepository<CategoryEntity, Integer> {
 }
--- src/main/java/com/aalifadesigns/matatatraveller/dao/CityDao.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/CityDao.java
@@ -2,6 +2,8 @@ package com.aalifadesigns.matatatraveller.dao;
 
 import com.aalifadesigns.matatatraveller.dao.entities.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityDao extends JpaRepository<CityEntity, Integer> {
 }
