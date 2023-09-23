fe3e41079f04c6ef7ffe19cf30532c11edd9be94
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-23 07:06:59 +0100

    CityDto

--- src/main/java/com/aalifadesigns/matatatraveller/model/CityDto.java
+++ src/main/java/com/aalifadesigns/matatatraveller/model/CityDto.java
@@ -1,9 +1,18 @@
 package com.aalifadesigns.matatatraveller.model;
 
 import lombok.*;
-
-
+//Using LOMBOK
+@NoArgsConstructor //empty constructor
+@AllArgsConstructor //a constructor using all attributes
+@Getter //for getters
+@Setter //for setters
+@ToString //to use ToString method
+@EqualsAndHashCode // equals and hash code
 
 public class CityDto {
-
+    private int cityId;
+    private String cityName;
+    private String cityCountry;
+    private String cityImg;
+    private String cityCurrency;
 }
