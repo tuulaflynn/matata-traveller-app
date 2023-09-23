29b5c127d38928ac4f8cc7bc6d89c85e921c2342
220057585SofiaBilal <Sofia.Bilal@city.ac.uk>
2023-09-23 19:17:57 +0100

    Completed Attraction dto,entity

--- src/main/java/com/aalifadesigns/matatatraveller/controller/AttractionController.java
+++ src/main/java/com/aalifadesigns/matatatraveller/controller/AttractionController.java
@@ -1,4 +1,5 @@
 package com.aalifadesigns.matatatraveller.controller;
 
 public class AttractionController {
+
 }
--- src/main/java/com/aalifadesigns/matatatraveller/dao/entities/AttractionEntity.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/entities/AttractionEntity.java
@@ -1,4 +1,48 @@
 package com.aalifadesigns.matatatraveller.dao.entities;
+import java.time.LocalDate;
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+@Entity
+@Table(name="attraction_details")
 public class AttractionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="attraction_id")
+    private int attractionId;
+
+    @Column(name="attraction_name")
+    private String attractionName;
+
+    @Column(name="attraction_description")
+    private String attractionDescription;
+
+    @Column(name="attraction_image")
+    private String attractionImage;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private CityEntity cityEntity;
 }
