3cd21b8fd6db0972351a4992d1969a7fa7ff99e9
Tuula Flynn <62618067+tuulaflynn@users.noreply.github.com>
2023-09-23 16:44:27 +0100

    Thread entity complete, all methods completed and tested

--- src/main/java/com/aalifadesigns/matatatraveller/dao/ThreadDao.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/ThreadDao.java
@@ -3,5 +3,9 @@ package com.aalifadesigns.matatatraveller.dao;
 import com.aalifadesigns.matatatraveller.dao.entities.ThreadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ThreadDao extends JpaRepository<ThreadEntity, Integer> {
+    List<ThreadEntity> findByThreadDate(LocalDate threadDate);
 }
--- src/main/java/com/aalifadesigns/matatatraveller/service/ThreadServiceImpl.java
+++ src/main/java/com/aalifadesigns/matatatraveller/service/ThreadServiceImpl.java
@@ -1,6 +1,7 @@
 package com.aalifadesigns.matatatraveller.service;
 
 import com.aalifadesigns.matatatraveller.dao.ThreadDao;
+import com.aalifadesigns.matatatraveller.dao.entities.CityEntity;
 import com.aalifadesigns.matatatraveller.dao.entities.ThreadEntity;
 import com.aalifadesigns.matatatraveller.model.CityDto;
 import com.aalifadesigns.matatatraveller.model.ThreadDto;
@@ -65,32 +66,80 @@ public class ThreadServiceImpl implements ThreadService {
 
     @Override
     public ThreadDto addThread(ThreadDto newThreadDto) {
+        // Copy dto into an entity
+        ThreadEntity newThreadEntity = new ThreadEntity();
+        CityEntity newCityEntity = new CityEntity();
+        BeanUtils.copyProperties(newThreadDto, newThreadEntity);
+        // Copy in the composite entity too
+        BeanUtils.copyProperties(newThreadDto.getCityDto(),newCityEntity);
+        newThreadEntity.setCityEntity(newCityEntity);
 
-        return null;
+        // Add the entity to the database. N.B. if the primary key is not unique, it will update the existing record with this entity
+        threadDao.saveAndFlush(newThreadEntity);
+        newThreadDto.setThreadId(newThreadEntity.getThreadId());
+        return newThreadDto;
     }
 
     @Override
     public List<ThreadDto> fetchByThreadDate(LocalDate threadDate) {
-        return null;
+        // Store the entities returned by the date search in a list of ThreadEntity objects.
+        // This list will be empty if there are no threads on the date 'threadDate'
+        List<ThreadEntity> allThreadsEntity = threadDao.findByThreadDate(threadDate);
+        // Collection of records (which have been stored in Dto objects) to be sent to the controller layer.
+        List<ThreadDto> allThreadsDto = new ArrayList<ThreadDto>();
+
+        // Transferring each record from an entity to a dto (including copying the record's composite entity).
+        allThreadsEntity.forEach(eachThreadEntity -> {
+            ThreadDto eachThreadDto = new ThreadDto();
+            CityDto eachCityDto = new CityDto();        // this is for the composite entity inside the thread entity
+            // Copy over everything
+            BeanUtils.copyProperties(eachThreadEntity, eachThreadDto);
+            // Copy over the composite property
+            BeanUtils.copyProperties(eachThreadEntity.getCityEntity(), eachCityDto);
+            // Set the composite property
+            eachThreadDto.setCityDto(eachCityDto);
+            // Add the dto to the collection allThreadsDto
+            allThreadsDto.add(eachThreadDto);
+        });
+        return allThreadsDto;
     }
 
     @Override
     public List<ThreadDto> fetchByCategoryId(int categoryId) {
+        // This is going to be achieved in fetch allThreads field of CategoryEntity
         return null;
     }
 
     @Override
     public List<ThreadDto> fetchByCityId(int cityId) {
+        // This is going to be achieved in fetch allThreads field of CityEntity
         return null;
     }
 
     @Override
     public ThreadDto updateThread(ThreadDto updateThreadDto) {
-        return null;
+        ThreadEntity updateThreadEntity = new ThreadEntity();
+        CityEntity updateCityEntity = new CityEntity();
+        BeanUtils.copyProperties(updateThreadDto, updateThreadEntity);
+        // Copy in the composite entity too
+        BeanUtils.copyProperties(updateThreadDto.getCityDto(),updateCityEntity);
+        updateThreadEntity.setCityEntity(updateCityEntity);
+
+        // update the entity in the database. N.B. if the primary key is unique, it will add a new record with this entity
+        threadDao.saveAndFlush(updateThreadEntity);
+        updateThreadDto.setThreadId(updateThreadEntity.getThreadId());
+        return updateThreadDto;
     }
 
     @Override
     public void deleteThread(int threadId) {
-
+        Optional<ThreadEntity> deleteThreadEntityOptional = threadDao.findById(threadId);
+        if (deleteThreadEntityOptional.isPresent()) {
+            threadDao.deleteById(threadId);
+        }
+        else {
+            // EDIT: add a custom exception here.
+            System.out.println("Error. Invalid thread id.");
+        }
     }
 }
