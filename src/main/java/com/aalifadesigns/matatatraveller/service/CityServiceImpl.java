a8fc7826cc587074d5345008c2fb65a304854be6
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-23 07:37:13 +0100

    CityServiceImpl setup - constructor DI

--- src/main/java/com/aalifadesigns/matatatraveller/service/CityServiceImpl.java
+++ src/main/java/com/aalifadesigns/matatatraveller/service/CityServiceImpl.java
@@ -1,4 +1,30 @@
 package com.aalifadesigns.matatatraveller.service;
 
-public class CityServiceImpl {
+import com.aalifadesigns.matatatraveller.dao.CityDao;
+import com.aalifadesigns.matatatraveller.model.CityDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CityServiceImpl implements CityService{
+
+    CityDao cityDao;
+
+    @Autowired //constructor Dependency Injection
+    public CityServiceImpl(CityDao cityDao) {
+        this.cityDao = cityDao;
+    }
+
+    @Override
+    public List<CityDto> fetchAllCities() {
+        return null;
+    }
+
+    @Override
+    public CityDto fetchACategory(int cityId) {
+        return null;
+    }
 }
