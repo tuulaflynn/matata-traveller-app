b8986b56f9051a893b597a93414922d3f5f96510
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-23 07:13:22 +0100

    CityEntity

--- src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CityEntity.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CityEntity.java
@@ -1,4 +1,26 @@
 package com.aalifadesigns.matatatraveller.dao.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city_details")
 public class CityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "city_country")
+    private String cityCountry;
+
+    @Column(name = "city_img")
+    private String cityImg;
+
+    @Column(name = "city_currency")
+    private String cityCurrency;
 }
+
