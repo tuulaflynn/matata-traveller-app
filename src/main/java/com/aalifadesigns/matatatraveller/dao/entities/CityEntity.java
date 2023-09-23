274319d03e890ce048ef268af8c4df27a198c24d
Gabriela-Patrichi <gabriela.patrichi@gmail.com>
2023-09-23 07:17:20 +0100

    CityEntity

--- src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CityEntity.java
+++ src/main/java/com/aalifadesigns/matatatraveller/dao/entities/CityEntity.java
@@ -2,12 +2,14 @@ package com.aalifadesigns.matatatraveller.dao.entities;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "city_details")
 public class CityEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-generated
     @Column(name = "city_id")
     private int cityId;
 
@@ -22,5 +24,15 @@ public class CityEntity {
 
     @Column(name = "city_currency")
     private String cityCurrency;
+
+    //ManyToMany relationship with Threads
+    //joinTable name specifies the table that connects these 2 tables, and the city_details becomes the owner
+    //the thread_details is the inverse
+    //JoinColumn/ inverseJoinColumn - on the FK column, which connects with the 3rd table
+    @ManyToMany
+    @JoinTable (name="city_thread",
+            joinColumns = @JoinColumn (name = "city_id"),
+            inverseJoinColumns = @JoinColumn(name = "thread_id"))
+    private List<ThreadEntity> allThreads; //collection of Threads
 }
 
