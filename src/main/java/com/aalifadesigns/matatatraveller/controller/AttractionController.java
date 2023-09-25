c71d988aa73754d282cd040bca0df6fc157edb65
220057585SofiaBilal <Sofia.Bilal@city.ac.uk>
2023-09-25 17:49:33 +0100

    Tested Attraction endpoints

--- src/main/java/com/aalifadesigns/matatatraveller/controller/AttractionController.java
+++ src/main/java/com/aalifadesigns/matatatraveller/controller/AttractionController.java
@@ -32,7 +32,7 @@ public class AttractionController {
     @Autowired
 
     // 1. fetch all attractions
-    // http://localhost:8080/api/attractions
+    // http://localhost:7474/api/attractions
     @GetMapping("/attractions")
     public ResponseEntity<List<AttractionDto>> fetchAllAttractions() {
         // here ResponseEntity wraps up the response body(1st argument) and the status code(2nd argument)
@@ -42,41 +42,14 @@ public class AttractionController {
     }
 
     // 2. fetch an attraction
-    // http://localhost:8080/api/attractions/1
+    // http://localhost:7474/api/attractions/1
     // this 1 is called a PathVariable with the name bid and should be extracted and store in a java variable/parameter(attractionId)
     @GetMapping("/attractions/{bid}")
     public ResponseEntity<AttractionDto> fetchAAttraction(@PathVariable("bid") int attractionId) {
         return new ResponseEntity<AttractionDto>(attractionService.fetchAAttraction(attractionId), HttpStatus.OK);
     }
 
-
-    // 3. add a attraction
-    // http://localhost:8080/attractions
-    // here the new attraction data to be inserted into the DB is sent along with the request body
-    // so what we should do is extract the new attraction data from the request body and store in the java method parmameter(newattraction)
-    // this is done with the help of @RequestBody
-    @PostMapping("/attractions")
-    public ResponseEntity<AttractionDto> addAttraction(@RequestBody AttractionDto newAttraction) {
-        return new ResponseEntity<AttractionDto>(attractionService.addAttraction(newAttraction), HttpStatus.OK);
-    }
-
-    // 4. update a attraction
-    // http://localhost:8080/attractions
-    @PutMapping("/attractions")
-    public ResponseEntity<AttractionDto> updateBook(@RequestBody AttractionDto updateAttraction) {
-        return new ResponseEntity<AttractionDto>(attractionService.updateAttraction(updateAttraction), HttpStatus.OK);
-    }
-
-    // 5. delete a attraction
-    // http://localhost:8080/api/attractions/1
-    @DeleteMapping("/attractions/{bid}")
-    public ResponseEntity<Void> removeAttraction(@PathVariable("bid") int attractionId) {
-        attractionService.removeAttraction(attractionId);
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
-
-
-    //6. fetch attractions by city
+    //3. http://localhost:7474/api/attractions/city/1
     @GetMapping("attractions/city/{cid}")
     public ResponseEntity<List<AttractionDto>> fetchAttractionsByCity(@PathVariable("cid") int cityId) {
         //fetch CityDto using cityService
@@ -87,5 +60,34 @@ public class AttractionController {
         }
         return new ResponseEntity<>(attractionService.fetchAttractionsByCity(cityDto), HttpStatus.OK);
     }
+
+
+    // 4. add an attraction
+    // http://localhost:7474/attractions
+    // here the new attraction data to be inserted into the DB is sent along with the request body
+    // so what we should do is extract the new attraction data from the request body and store in the java method parmameter(newattraction)
+    // this is done with the help of @RequestBody
+    @PostMapping("/attractions")
+    public ResponseEntity<AttractionDto> addAttraction(@RequestBody AttractionDto newAttraction) {
+        return new ResponseEntity<AttractionDto>(attractionService.addAttraction(newAttraction), HttpStatus.OK);
+    }
+
+    // 5. update an attraction
+    // http://localhost:7474/attractions
+    @PutMapping("/attractions")
+    public ResponseEntity<AttractionDto> updateBook(@RequestBody AttractionDto updateAttraction) {
+        return new ResponseEntity<AttractionDto>(attractionService.updateAttraction(updateAttraction), HttpStatus.OK);
+    }
+
+    // 6. delete an attraction
+    // http://localhost:7474/api/attractions/1
+    @DeleteMapping("/attractions/{bid}")
+    public ResponseEntity<Void> removeAttraction(@PathVariable("bid") int attractionId) {
+        attractionService.removeAttraction(attractionId);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+
+
 }
 
