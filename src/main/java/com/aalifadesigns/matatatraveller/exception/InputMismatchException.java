7e720714636ea26713298291d048c631af92b2fe
Tuula Flynn <62618067+tuulaflynn@users.noreply.github.com>
2023-09-26 11:32:32 +0100

    Deleted InputMismatchExcpetion

--- src/main/java/com/aalifadesigns/matatatraveller/exception/InputMismatchException.java
+++
@@ -1,8 +0,0 @@
-package com.aalifadesigns.matatatraveller.exception;
-
-public class InputMismatchException extends RuntimeException{
-    @Override
-    public String getMessage(){
-        return ("Input data does not match the expected format");
-    }
-}
