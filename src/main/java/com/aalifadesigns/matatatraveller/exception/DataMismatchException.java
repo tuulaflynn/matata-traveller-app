a837bcc31502fdacf2198d04e917ae3e490edbc0
Tuula Flynn <62618067+tuulaflynn@users.noreply.github.com>
2023-09-26 11:31:29 +0100

    Updated exception package, removed some classes

--- src/main/java/com/aalifadesigns/matatatraveller/exception/DataMismatchException.java
+++
@@ -1,8 +0,0 @@
-package com.aalifadesigns.matatatraveller.exception;
-
-public class DataMismatchException extends RuntimeException{
-    @Override
-    public String getMessage() {
-        return ("Data stored in the system does not match expected values");
-    }
-}
